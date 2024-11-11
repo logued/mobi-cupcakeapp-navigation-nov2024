5a2decb1f8f3d555bbcfaa10706935fe58f9713c
Dermot Logue <dermot.logue@dkit.ie>
2024-11-11 21:25:55 +0000

    Initial commit

---
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -0,0 +1,22 @@
+/*
+ * DL Annotated Nov 2024
+ */
+package com.example.cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.cupcake.ui.theme.CupcakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                CupcakeApp()
+            }
+        }
+    }
+}
